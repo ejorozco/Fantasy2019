1d82cac2940c6cb1b28952957ccca8242cd38533
Eric Orozco <wookiee323@gmail.com>
2020-05-25 17:25:34 -0700

    Building Angular App. Masthead and Home page integration

--- src/interface/webapp/src/app/app.module.ts
+++ src/interface/webapp/src/app/app.module.ts
@@ -1,18 +1,35 @@
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { ReactiveFormsModule }    from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
+// used to create fake backend
+//import { fakeBackendProvider } from './_helpers';
+
+import { AppComponent }  from './app.component';
+import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { MastHeadComponent } from './masthead/masthead.component';
+// import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        AppRoutingModule
+    ],
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        MastHeadComponent
+    ],
+    // providers: [
+    //     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    //     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+    // ],
+    bootstrap: [AppComponent]
 })
+
 export class AppModule { }
